4e3bd93fda5579e185cd520eabbea3c10d3bae4c
ivan-pua <qieshang@gmail.com>
2024-10-13 07:20:44 +1100

    frontend

--- main.py
+++ main.py
@@ -10,95 +10,89 @@ from transformers import AutoTokenizer, pipeline, AutoModelForQuestionAnswering
 load_dotenv() # takes variables from .env file
 
 # 1. Loaded PDF
-print("1. Loading PDF files...")
-file_path = "files/databricks-llm.pdf"
-loader = PyPDFLoader(file_path)
+def generate_response(query, uploaded_files):
+    print("1. Loading PDF files...")
+    # file_path = "files/databricks-llm.pdf"
+    loader = PyPDFLoader(f"files/{uploaded_files.name}")
 
-documents = loader.load()
+    documents = loader.load()
 
-print(len(documents))
-print(documents[0].page_content[0:100])
-print(documents[0].metadata)
+    print(len(documents))
+    print(documents[0].page_content[0:100])
+    print(documents[0].metadata)
 
-# 2. Split document into chunks - for precise matching and fitting context window
-print("\n2. Chunking documents... ")
+    # 2. Split document into chunks - for precise matching and fitting context window
+    print("\n2. Chunking documents... ")
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-split_documents = text_splitter.split_documents(documents=documents)
-print(len(split_documents))
-print(split_documents[0])
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    split_documents = text_splitter.split_documents(documents=documents)
+    print(len(split_documents))
+    print(split_documents[0])
 
-voyage_key = os.getenv("VOYAGE_API_KEY")
+    voyage_key = os.getenv("VOYAGE_API_KEY")
 
-# 3. Create embeddings
-print("\n3. Create embeddings...")
-embeddings = VoyageAIEmbeddings(
-    voyage_api_key=voyage_key, model="voyage-2",
-)
-
-# sample_docs = [
-#     "John works in TPG Telecom."
-# ]
-
-# documents_embds = embeddings.embed_documents(sample_docs)
-
-# print(documents_embds[0][:5])
-
-# 4. Save embeddings in vector store
-vector_store_folder_path = "faiss_index"
-if  os.path.isdir(vector_store_folder_path):
-
-    print("\n4. Loading existing vector store... ")
-    vectordb = FAISS.load_local(
-        vector_store_folder_path,
-        embeddings,
-        allow_dangerous_deserialization=True
+    # 3. Create embeddings
+    print("\n3. Create embeddings...")
+    embeddings = VoyageAIEmbeddings(
+        voyage_api_key=voyage_key, model="voyage-2",
     )
 
-else:
-    print("\n4. Create new vector store... ")
-    vectordb = FAISS.from_documents(split_documents, embeddings)
-    vectordb.save_local(vector_store_folder_path)
+    # 4. Save embeddings in vector store
+    vector_store_folder_path = "faiss_index"
+    if  os.path.isdir(vector_store_folder_path):
 
-# 5. Retriever
-print("\n5. Creating a retriever... ")
-# by default uses cosine similarity
-retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+        print("\n4. Loading existing vector store... ")
+        vectordb = FAISS.load_local(
+            vector_store_folder_path,
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
 
-query = "How are companies using LLMs?"
-print(f"Question: {query}\n")
+    else:
+        print("\n4. Create new vector store... ")
+        vectordb = FAISS.from_documents(split_documents, embeddings)
+        vectordb.save_local(vector_store_folder_path)
 
-# retrieve the most relevant documents
-docs = retriever.invoke(query)
+    # 5. Retriever
+    print("\n5. Creating a retriever... ")
+    # by default uses cosine similarity
+    retriever = vectordb.as_retriever(search_kwargs={"k": 3})
 
-context = '\n\n'.join([doc.page_content for doc in docs])
-print(context)
+    # query = "How are companies using LLMs?"
+    print(f"Question: {query}\n")
 
-# 6. Set up LLM
-# Specify the model name you want to use
-model_name = "deepset/tinyroberta-squad2"
+    # retrieve the most relevant documents
+    docs = retriever.invoke(query)
 
-# # Download a tokenizer object by loading the pretrained "Intel/dynamic_tinybert" tokenizer.
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
+    context = '\n\n'.join([doc.page_content for doc in docs])
+    print(context)
 
-# # Download a question-answering model object by loading the pretrained "Intel/dynamic_tinybert" model.
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    # 6. Set up LLM
+    # Specify the model name you want to use
+    model_name = "deepset/tinyroberta-squad2"
 
-# Load the tokenizer associated with the specified model
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # # Download a tokenizer object by loading the pretrained "Intel/dynamic_tinybert" tokenizer.
+    # tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Define a question-answering pipeline using the model and tokenizer
-question_answerer = pipeline(
-    task="question-answering", 
-    model=model, 
-    tokenizer=tokenizer,
-    temperature=0.5,
-    max_new_tokens=256,  
-    repetition_penalty=1.5,
-    do_sample=True,
-    device=0
-)
+    # # Download a question-answering model object by loading the pretrained "Intel/dynamic_tinybert" model.
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 
-result = question_answerer(question=query, context=context)
-print(f"\nAnswer: '{result['answer']}', score: {round(result['score'], 4)}, start: {result['start']}, end: {result['end']}")
+    # Load the tokenizer associated with the specified model
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+    # Define a question-answering pipeline using the model and tokenizer
+    question_answerer = pipeline(
+        task="question-answering", 
+        model=model, 
+        tokenizer=tokenizer,
+        temperature=0.5,
+        max_new_tokens=256,  
+        repetition_penalty=1.5,
+        do_sample=True,
+        device=0
+    )
+
+    result = question_answerer(question=query, context=context)
+    print(f"\nAnswer: '{result['answer']}', score: {round(result['score'], 4)}, start: {result['start']}, end: {result['end']}")
+
+    return result['answer']
